85a8e351892244f9f452724c69c500f185b77806
jovanidash21 <jwardash21@gmail.com>
2017-01-12 18:25:20 +0800

    Add Function

--- IntegerFactorization/Module1.vb
+++ IntegerFactorization/Module1.vb
@@ -1,7 +1,22 @@
 ﻿Module Module1
 
     Sub Main()
-
+        Console.Write("Please Enter an Integer: ")
+        Dim input As String = Console.ReadLine()
+        Dim number As Integer
+        If Integer.TryParse(input, number) Then
+            Console.WriteLine("The Factors are: ")
+            For x = 1 To number
+                If number Mod x = 0 Then
+                    Console.WriteLine(x)
+                End If
+            Next
+            Main()
+        Else
+            Console.WriteLine("Invalid Input")
+            Main()
+        End If
+        Console.ReadLine()
     End Sub
 
 End Module
